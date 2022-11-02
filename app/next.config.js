14ef37a23f48d3df9ea2d7045eea327d14df29c2
amy-mavis-qin <amyqin501@gmail.com>
2022-11-02 12:21:53 -0400

    update next config

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isDev = process.env.NODE_ENV === 'development'
+
 const nextConfig = {
-  assetPrefix: './',
+  assetPrefix: isDev ? './' : 'https://csclub.uwaterloo.ca/~uwblockchain/',
   reactStrictMode: true,
   swcMinify: true, 
   images: {
