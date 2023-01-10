d1d1b9f315d2c67c0d6565275f8b5dbb9b80a3c7
amy-mavis-qin <amyqin501@gmail.com>
2023-01-10 16:19:38 -0500

    Add connect page

---
+++ resources/views/components/partnership.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="{{asset('css/Partnerships.css')}}">
+<div class="partnership">
+    <a href={{$link}}><img src={{$src}} class="partner_img" width={64} height={64}></img></a>
+    <a href={{$link}} class='partner_name'><h1>{{$name}}</h1></a>
+</div>
