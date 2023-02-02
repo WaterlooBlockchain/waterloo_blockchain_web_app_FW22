2268c734246b871de3ac96591d1a81a8a3ce7be4
Johnatan Gao <dev@johnatangao.com>
2023-02-02 01:28:51 -0500

    Create database migration & seeder + image optimisation

--- database/migrations/2023_02_02_041519_create_blogs_table.php
+++ database/migrations/2023_02_02_041519_create_blogs_table.php
@@ -3,18 +3,22 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
 
 class CreateBlogsTable extends Migration
 {
     public function up()
     {
         Schema::create('blogs', function (Blueprint $table) {
-            $table->id();
+            $table->uuid()->default(DB::raw('uuid()'));
+            $table->text('title');
             $table->boolean('isFeatured');
-            $table->text('image');
+            $table->longText('image');
             $table->text('tags');
             $table->longText('content');
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $sql = file_get_contents(database_path('seeds/blogs.sql'));
+        DB::unprepared($sql);
     }
 }
