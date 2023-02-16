93091b6bccfe55202059812fbabee8e23f7c14ea
Xavier D <xaviermdmello@gmail.com>
2023-02-16 08:56:29 -0500

    responsive footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -9,14 +9,17 @@
             <p class="uwaterloo">UWaterloo</p>
             <div class="line">
                 <h1 class="blockchain">Blockchain</h1>
-                <p class="c">© 2022</p>
+                <p class="c">© 2023</p>
             </div>
+                
+    
+
         </div>
     </div>
     <div class="socials">
         <div class="socials_button"><a href='https://mobile.twitter.com/uw_blockchain' target="_blank" rel="noreferrer"><i class="fa-brands fa-twitter fa-2xl icons"></i></a></div>
         <div class="socials_button"><a href='https://www.instagram.com/blockchainuw/' target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram fa-2xl icons"></i></a></div>
-        <div class="socials_button"><a href='https://www.linkedin.com/company/uwblockchainclub/?originalSubdomain=ch' target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin fa-2xl icons"></i><a></div>
+        <div class="socials_button"><a href='https://www.linkedin.com/company/uwblockchainclub/?originalSubdomain=ch' target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin fa-2xl icons"></i></a></div>
         <div class="socials_button"><a href='https://discord.gg/YCBA3n7Xn8' target="_blank" rel="noreferrer"><i class="fa-brands fa-discord fa-2xl icons"></i></a></div>
     </div>
 </div>
