0371de910c88d71c8405933e0a4ba4260bf576a1
Xavier D <xaviermdmello@gmail.com>
2023-02-16 06:55:16 -0500

    responsive about section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@
         Our mission is to bring together a community of passionate individuals to explore and work in blockchain ecosystems like Web3 companies or their own startup. We facilitate and promote growth through events, including technical workshops, meetups with industry experts, and supporting student attendance at conferences. We are nurturing Waterloo&apos;s blockchain community, creating spaces for new connections and exchanging ideas through events. 
         </p>
     </div>
-    <div class="panel_section">
+    <div class="panel_section masked-overflow">
             @include('components.panel', [
                 'icon' => 'fa-solid fa-user-group fa-2xl',
                 'header' => 'Work with Us',
@@ -50,13 +50,13 @@
                 'content' =>'Blockchain club creates an environment for organizations to connect with talented individuals passionate about the crypto space. We offer a direct line of communication to our community which can help disseminate projects, offers, and events.'
             ])
             @include('components.panel', [
-                'icon' => 'fa-solid fa-newspaper fa-2xl',
+                'icon' => 'fa-solid fa-hammer fa-2xl',
                 'header' => 'Learn with Us',
                 'title' => 'Workshops',
                 'content' =>'Our current events are primarily targeted at skill development, examples include hackathons, CTFs, workshops and educational seminars. We average around 50 student participants for in-person events, providing educational and technical resources to fellow developers (new and veterans).'
             ])
             @include('components.panel', [
-                'icon' => 'fa-solid fa-hammer fa-2xl',
+                'icon' => 'fa-solid fa-newspaper fa-2xl',
                 'header' => 'Educate with Us',
                 'title' => 'Research',
                 'content' =>'We create value for the broader blockchain community through research and development, our membership has a wide range of technical backgrounds, with some developing start-ups in the Defi and NFT space, and some who were just onboarded to the Web3 space. DYOR!'
