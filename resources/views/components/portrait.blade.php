091b36d0315fa6e25ec9f51f59e689b8d5d4c6ee
Xavier D <xaviermdmello@gmail.com>
2023-02-19 00:09:55 -0500

    added links to meet the team section

--- resources/views/components/portrait.blade.php
+++ resources/views/components/portrait.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="{{asset('css/Portrait.css')}}">
-<div class="portrait">
+<a class="portrait" href={{$link}} target="_blank" rel="noopener noreferrer">
     <img class="portrait_image" src={{$src}}></img>
     <b class="portrait_name">{{$name}}</b>
     <i class="portrait_title">{{$title}}</i>
-</div>
+</a>
