200c9d6995ccfcc7332bd420b0a9f54299e23285
Xavier D <xaviermdmello@gmail.com>
2023-02-25 23:10:15 -0500

    update responsive design globally

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,33 +3,34 @@
 <link rel="stylesheet" href="{{asset('css/Blog.css')}}">
 
 <div class="blog_container">
-  <h1 class="title">Blog</h1>
-  @foreach ($blog_posts as $post)
+  <h1 class="featured_title title">Blog</h1>
+
+  <div class="featured_grid">
+      @foreach ($blog_posts as $post)
     @if($post->isFeatured == 1)
-      <div class="featured_container">
-      <div class="featured">
-        <p>FEATURED</p>
-      </div>
-      <div class="content_container">
-        <h1 class="blog_title">{{$post->title}}</h1>
-        <div class="blog_content">
-          {{$post->content}}
-        </div>
-      </div>
-      <div class="img_container"><img class="featured_img" src="{{$post->image}}" width=275 height=250></img></div>
-    </div>
+        @include('components.post', [
+          'theme' => 'dark',
+          'src' => $post->image, 
+          'title' => $post->title,
+          'tags' => explode(",", $post->tags),
+          'content' => $post->content,
+          'isFeatured' => $post->isFeatured,
+        ])
     @endif
   @endforeach
+  </div>
+
   
   <div class="blog_grid">
     @foreach ($blog_posts as $post)
       @if ($post->isFeatured != 1)
         @include('components.post', [
           'theme' => 'dark',
-          'src' => $post->image,
+          'src' => $post->image, 
           'title' => $post->title,
           'tags' => explode(",", $post->tags),
-          'content' => $post->content
+          'content' => $post->content,
+          'isFeatured' => $post->isFeatured,
         ])
       @endif
     @endforeach
